7d2d28c3961e4ec9e1adfcba2d936cbc5e300767
Jorge-Mateus <mateusnascimento07@hotmail.com>
2021-09-21 18:38:13 -0300

    servidor completo

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ServidorWEB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var servidorHttp = new ServidorHttp();
+            
+        }
+    }
+}
